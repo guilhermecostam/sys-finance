9d5627548c9308b0b31c9d0f3f444007e7156fa5
guilhermecostam <guicm007@gmail.com>
2022-05-13 10:27:42 -0300

    feat: add endpoint for health server

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,12 +3,14 @@ package http
 import (
 	"net/http"
 
+	"github.com/guilhermecostam/sys-finance/adapter/http/actuator"
 	"github.com/guilhermecostam/sys-finance/adapter/http/transaction"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransactions)
+	http.HandleFunc("/health", actuator.Health)
 
 	http.ListenAndServe(":8000", nil)
 }
