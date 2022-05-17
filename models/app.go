c7186e98f0fbaf16bbee090f176266e7621c1cab
Guilherme Medeiros <guilherme.medeiros@codificar.com.br>
2022-05-16 22:36:46 -0300

    feat: add new server init and remodelate model for new transactions

--- models/app.go
+++ models/app.go
@@ -1,16 +1,35 @@
 package models
 
 import (
-	"time"
+	"fmt"
+	"net/http"
 )
 
 // Transaction is a struct to the transaction model
 type Transaction struct {
-	Title     string    `json:"title"`
-	Amount    float32   `json:"amount"`
-	Type      int       `json:"type"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        int     `json:"id"`
+	Title     string  `json:"title"`
+	Amount    float32 `json:"amount"`
+	CreatedAt string  `json:"created_at"`
 }
 
 // Transactions is a array of Transaction struct
-type Transactions []Transaction
+type TransactionList struct {
+	Transactions []Transaction `json:"transactions"`
+}
+
+func (t *Transaction) Bind(r *http.Request) error {
+	if t.Title == "" {
+		return fmt.Errorf("name is a required field")
+	}
+
+	return nil
+}
+
+func (*TransactionList) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (*Transaction) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
